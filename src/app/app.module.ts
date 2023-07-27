2bd0a0498ed31bff3f5fb3e1ef6b368c23c7b1a8
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-07-27 16:34:40 +0100

    tofunmiChanges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SectionWhatYouWillLearnComponent } from './section-what-you-will-learn/section-what-you-will-learn.component';
+import { SectionPastStudentsComponent } from './section-past-students/section-past-students.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SectionWhatYouWillLearnComponent,
+    SectionPastStudentsComponent
   ],
   imports: [
     BrowserModule,
