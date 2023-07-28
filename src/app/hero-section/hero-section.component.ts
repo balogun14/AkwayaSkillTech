fb84834bad5cca049c2eca7b7445925b981e419c
balogun14 <awwalbalogun06@gmail.com>
2023-07-28 04:34:07 +0100

    i added my parts

---
+++ src/app/hero-section/hero-section.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  templateUrl: './hero-section.component.html',
+  styleUrls: ['./hero-section.component.css']
+})
+
+
+export class HeroSectionComponent implements OnInit {
+  hours!: number;
+  minutes!: number;
+  seconds!: number;
+
+  ngOnInit() {
+    const countDownDate = new Date();
+    countDownDate.setHours(23);
+    countDownDate.setMinutes(59);
+    countDownDate.setSeconds(0);
+
+    setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate.getTime() - now;
+
+      this.hours = Math.floor(distance / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    }, 1000);
+  }
+}
