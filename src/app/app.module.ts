779fa73968df6cf304ecc981796e0e6525ee23f6
Bolaji Oluwatofunmi Ashraf <Tofunmibolaji13@gmail.com>
2023-07-28 10:58:43 +0100

    corrections made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { SectionWhatYouWillLearnComponent } from './section-what-you-will-learn/section-what-you-will-learn.component';
 import { SectionPastStudentsComponent } from './section-past-students/section-past-students.component';
-
-import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { OurMentorComponent } from './our-mentor/our-mentor.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
- Tofunmi_branch
+    HeroSectionComponent,
     SectionWhatYouWillLearnComponent,
     SectionPastStudentsComponent,
-    HeroSectionComponent,
     OurMentorComponent
   ],
   imports: [
